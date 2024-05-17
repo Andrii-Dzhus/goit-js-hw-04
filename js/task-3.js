729e67d5678927b4fa175738c1c1ea3e20147896
Andrii Dzhus <158450996+Andrii-Dzhus@users.noreply.github.com>
2024-05-17 11:41:13 +0300

    11:40

--- js/task-3.js
+++ js/task-3.js
@@ -1,24 +1,17 @@
 const profile = {
-username: "Jacob",
-  playTime: 300,
-
-  getInfo(){
-    return `${this.username} has ${this.playTime} active hours!`
-},
+    username: "Jacob",
+    playTime: 300,
 
 changeUsername(newName){
     this.username = newName;
 },
-updatePlayTime(hours)(
-    
-)
+updatePlayTime(hours){
+    this.playTime +=hours;
+},
+getInfo(){
+    return `${this.username} has ${this.playTime} active hours!`
 }
-
-// 
-
-// updatePlayTime(hours)
-
-
+}
 
 console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
